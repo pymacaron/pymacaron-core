6c2670c9773687000463e27292a45058f68f04a5
erwan <erwan@spotify.com>
2016-02-08 13:56:26 +0100

    Readying setup.* for pypi

--- setup.py
+++ setup.py
@@ -28,10 +28,36 @@ if not version:
 print("version: %s" % version)
 
 setup(name='klue-client-server',
-      version=version,
-      description='Library of code common to all pnt backends',
-      url='https://bitbucket.org/peopleandthings/backend-common',
+      version='0.0.1',
+      url='https://github.com/erwan-lemonnier/klue-client-server',
+      license='BSD',
       author='Erwan Lemonnier',
-      author_email='erwan@lemonnier.se',
-      packages=['pnt_common', 'pnt_common.swagger'],
-      zip_safe=False)
+    author_email='erwan@lemonnier.se',
+    description='Swagger + Flask + Grequests + Bravado = Client/Server auto-spawning',
+    install_requires=[
+        'flask',
+        'grequests',
+        'bravado-core',
+        'pyyaml'
+    ],
+    tests_require = [
+        'nose',
+        'mock',
+        'responses',
+        'pep8'
+    ],
+    test_suite = 'nose.collector',
+    packages=['klue', 'klue.swagger'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
