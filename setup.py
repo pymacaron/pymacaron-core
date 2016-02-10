7532bd85783da1bc41c4593c96e401358f2c4016
erwan <erwan@spotify.com>
2016-02-10 22:29:45 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='klue-client-server',
-      version='0.0.4',
+      version='0.0.5',
       url='https://github.com/erwan-lemonnier/klue-client-server',
       license='BSD',
       author='Erwan Lemonnier',
