fa14013bb8378abb1ab7b9101c027ec59b7ddfdd
erwan <erwan@spotify.com>
2016-02-15 12:53:46 +0100

    Added proper release script: publish.sh

--- setup.py
+++ setup.py
@@ -1,8 +1,35 @@
 from setuptools import setup
+import sys
+import os
+
+version = None
+
+if sys.argv[-2] == '--version' and 'sdist' in sys.argv:
+    version = sys.argv[-1]
+    sys.argv.pop()
+    sys.argv.pop()
+
+if 'sdist' in sys.argv and not version:
+    raise Exception("Please set a version with --version x.y.z")
+
+if not version:
+    if 'sdist' in sys.argv:
+        raise Exception("Please set a version with --version x.y.z")
+    else:
+        path_pkg_info = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'PKG-INFO')
+        if os.path.isfile(path_pkg_info):
+            with open(path_pkg_info, 'r')as f:
+                for l in f.readlines():
+                    if 'Version' in l:
+                        _, version = l.split(' ')
+        else:
+            print("WARNING: cannot set version in custom setup.py")
+
+print("version: %s" % version)
 
 setup(
     name='klue-client-server',
-    version='0.0.11',
+    version=version,
     url='https://github.com/erwan-lemonnier/klue-client-server',
     license='BSD',
     author='Erwan Lemonnier',
