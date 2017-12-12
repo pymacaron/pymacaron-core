2dd9b2359111b85b44a825377675ec8fe37361ca
Erwan Lemonnier <erwan@lemonnier.se>
2017-12-12 12:55:36 +0100

    Use a more recent bravado-core

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     description='Swagger + Flask + Bravado = Client/Server auto-spawning',
     install_requires=[
         'flask==0.10.1',
-        'bravado-core==4.9.1',
+        'bravado-core==4.11.0',
         'pyyaml'
     ],
     tests_require=[
